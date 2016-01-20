88522d43d3f4841230f3a2d154432e6cf91cd203
ferron <ferronrsmith@gmail.com>
2016-01-20 10:25:26 -0400

    initial commit

---
+++ test/buyan-amqp-ftest.js
@@ -0,0 +1,35 @@
+/*
+ * File: test/logstash-amqp-test.js
+ * Description: Test script for the logstash-amqp library
+ */
+
+var bunyan = require('bunyan'),
+    bunyanamqp = require("../lib/bunyan-amqp");
+
+var amq_stream = bunyanamqp.createStream({
+    login : 'admin',
+    password : 'UbpdMksr0ons',
+    exchange : {
+        routingKey : 'logs'
+    }
+}).on('connect', function () {
+    console.log("Connected to amqp");
+}).on('close', function (e) {
+    console.log("Closed connection to amqp");
+}).on('error', console.log);
+
+
+var log = bunyan.createLogger({
+    name : 'example',
+    streams : [{
+        level : 'debug',
+        stream : process.stdout
+    }, {
+        level : 'debug',
+        type : 'raw',
+        stream : amq_stream
+    }]
+});
+
+log.info("this is just a test");
+log.debug("this is a sample debug statement");
---
+++ test/buyan-amqp-test.js
@@ -0,0 +1,20 @@
+/*
+ * File: test/logstash-amqp-test.js
+ * Description: Test script for the logstash-amqp library
+ */
+
+var bunyan = require('bunyan'),
+    bunyanamqp = require("../lib/bunyan-amqp"),
+    chai = require('chai'),
+    expect = require('chai').expect,
+    assert = require('assert');
+
+describe('bunyan-logstash-amqp', function () {
+    it('should create a bunyan stream', function (done) {
+        var s = bunyanamqp.createStream();
+        expect(s).to.be.an.instanceof(bunyanamqp.AmqStream);
+        expect(s).to.respondTo('write');
+        done();
+    });
+
+});
